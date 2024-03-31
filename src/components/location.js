7e15faebce7464c90ed0d8b17d038c7c438adcbd
Pranjal Aggarwal <54495980+pranjalagg@users.noreply.github.com>
2024-03-31 16:29:11 -0600

    Code Formatting

--- src/components/location.js
+++ src/components/location.js
@@ -1,47 +1,61 @@
-import usePlacesAutoComplete, {getGeocode, getLatLng} from "use-places-autocomplete"
+import usePlacesAutoComplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 import Suggestion from "./suggestion";
 
+const Location = ({ setOffice }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutoComplete();
 
-const Location = ({setOffice}) =>{
+  const renderSuggestions = () => {
+    if (status === "OK") {
+      return (
+        <div>
+          {data.map((suggestion, index) => (
+            <Suggestion
+              key={index}
+              suggestion={suggestion}
+              index={index}
+              handleSelectSuggestion={handleSelectSuggestion}
+            ></Suggestion>
+          ))}
+        </div>
+      );
+    }
+  };
 
+  const handleSelectSuggestion = async (suggestion) => {
+    setValue(suggestion.description, false);
+    clearSuggestions();
 
-    
-    const {ready, value, setValue, suggestions: {status, data}, clearSuggestions} = usePlacesAutoComplete();
+    try {
+      const results = await getGeocode({ address: suggestion.description });
+      const { lat, lng } = await getLatLng(results[0]);
+      setOffice({ lat, lng });
+    } catch (error) {
+      console.error("Error selecting suggestion:", error);
+    }
+  };
 
-    const renderSuggestions = () => {
-        if (status === "OK") {
-            return (
-                <div>
-                    {data.map((suggestion, index) => (
-                            <Suggestion key = {index} suggestion={suggestion} index={index} handleSelectSuggestion={handleSelectSuggestion}></Suggestion>
-                    ))}
-                </div>
-            );
-        }
-    };
-    
-    const handleSelectSuggestion = async (suggestion) => {
-        setValue(suggestion.description, false);
-        clearSuggestions();
-
-        try {
-            const results = await getGeocode({ address: suggestion.description });
-            const { lat, lng } = await getLatLng(results[0]);
-            setOffice({ lat, lng });
-        } catch (error) {
-            console.error('Error selecting suggestion:', error);
-        }
-    };
-
-    return (
-            <div className="location">
-                <center>
-                    <input value = {value} onChange={(e) => setValue(e.target.value)} disabled={!ready} placeholder="Search an address"/>
-                    {renderSuggestions()}
-                </center>
-            </div>
-    )
-
-}
+  return (
+    <div className="location">
+      <center>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!ready}
+          placeholder="Search an address"
+        />
+        {renderSuggestions()}
+      </center>
+    </div>
+  );
+};
 
 export default Location;
