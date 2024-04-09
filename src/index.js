f836ad45529930487b644b46c782b3e9aa977f67
Deepanshu Sachdeva <56470354+deepanshusachdeva5@users.noreply.github.com>
2024-04-08 20:22:54 -0600

    Login Page added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom'
+
+import Login from "./login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID}>
+  <Router>
+    <Routes>
+      <Route element={<Login/>} exact path="/" />
+      <Route element={<App/>} path="/app" />
+    </Routes>
+  </Router>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
