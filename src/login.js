5f9a8d404492546345ccaaaea1fa1b94f937606e
Deepanshu Sachdeva <56470354+deepanshusachdeva5@users.noreply.github.com>
2024-04-08 20:24:58 -0600

    Prettier run

--- src/login.js
+++ src/login.js
@@ -1,49 +1,46 @@
-import React from  'react';
-import { GoogleLogin } from '@react-oauth/google';
-import { useNavigate} from 'react-router-dom';
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const responseGoogle = (response) => {
-      if (response.profileObj) { // if login is successful
-        navigate('/app'); // redirect to App component
-      }
-      navigate('/app');
+  const responseGoogle = (response) => {
+    if (response.profileObj) {
+      // if login is successful
+      navigate("/app"); // redirect to App component
     }
+    navigate("/app");
+  };
 
-    return (
-        <div>
-        <center>
+  return (
+    <div>
+      <center>
         <h1>Login</h1>
         <form>
-            <label>
+          <label>
             Username:
             <input type="text" name="username" />
-            </label>
-            <br />
-            <label>
+          </label>
+          <br />
+          <label>
             Password:
             <input type="password" name="password" />
-            </label>
-            <br />
-            <button type="submit">Login</button>
+          </label>
+          <br />
+          <button type="submit">Login</button>
         </form>
-        
-            Or
-            <br />
-            <GoogleLogin
-                onSuccess={responseGoogle}
-                onError={() => {
-                    console.log('Login Failed');
-                }}
-            />
-        </center>
-
-        </div>
-    );
-
+        Or
+        <br />
+        <GoogleLogin
+          onSuccess={responseGoogle}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </center>
+    </div>
+  );
 };
 
 export default Login;
