c2370e6d0711a93337000dbb122016568dace9f0
Grace Waida <grwa6713@colorado.edu>
2024-04-14 03:48:19 -0600

    updated login page

--- src/login.js
+++ src/login.js
@@ -61,11 +61,11 @@ const Login = () => {
 
   const responseGoogle = (response) => {
     if (response.profileObj) {
-        // if login is successful
-        navigate("/app"); // redirect to App component
-      }
-      navigate("/app");
-    };
+      // if login is successful
+      navigate("/app"); // redirect to App component
+    }
+    navigate("/app");
+  };
 
   const handleSignUpClick = () => {
     //will move to the signup.js when sign up button is called
@@ -73,35 +73,103 @@ const Login = () => {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "calc(100% - 1in)", padding: "10px", marginBottom: "20px"}}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "calc(100% - 1in)",
+          padding: "10px",
+          marginBottom: "20px",
+        }}
+      >
         {/*Helps with the alignment so that it is not flush with the side*/}
         <div style={{ marginLeft: "0.15in" }}>
-          <img src={LogoSafeMapWritten} alt="SafeMap Logo" style={{ height: "45px" }} />
+          <img
+            src={LogoSafeMapWritten}
+            alt="SafeMap Logo"
+            style={{ height: "45px" }}
+          />
         </div>
-        <button style={{ background: "#2a5ba6", color: "white", border: "none", borderRadius: "8px", padding: "10px 20px", cursor: "pointer", fontSize: "16px" }} onClick={handleSignUpClick}>Sign Up</button> {/* Increased button size */}
+        <button
+          style={{
+            background: "#2a5ba6",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            padding: "10px 20px",
+            cursor: "pointer",
+            fontSize: "16px",
+          }}
+          onClick={handleSignUpClick}
+        >
+          Sign Up
+        </button>{" "}
+        {/* Increased button size */}
       </div>
       {/*Alignment of the imagw*/}
-      <div style={{ width: "300px", textAlign: "center"}}>
-        <img src={LogoSafeMap1} alt="Your Image" style={{ width: "130%", marginBottom: "20px", marginLeft: "-45px"}} />
+      <div style={{ width: "300px", textAlign: "center" }}>
+        <img
+          src={LogoSafeMap1}
+          alt="Your Image"
+          style={{ width: "130%", marginBottom: "20px", marginLeft: "-45px" }}
+        />
         <h1>Sign In</h1>
         <form>
           <div style={{ marginBottom: "10px" }}>
-            <input type="text" placeholder="Email Address" name="username" style={{ width: "100%", padding: "10px", borderRadius: "5px" }} />
+            <input
+              type="text"
+              placeholder="Email Address"
+              name="username"
+              style={{ width: "100%", padding: "10px", borderRadius: "5px" }}
+            />
           </div>
           <div style={{ marginBottom: "10px" }}>
-            <input type="password" placeholder="Password" name="password" style={{ width: "100%", padding: "10px", borderRadius: "5px" }} />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              style={{ width: "100%", padding: "10px", borderRadius: "5px" }}
+            />
           </div>
-          <button type="submit" style={{ background: "#2a5ba6", color: "white", border: "none", borderRadius: "5px", padding: "8px 15px", cursor: "pointer" }}>Sign In</button>
+          <button
+            type="submit"
+            style={{
+              background: "#2a5ba6",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              padding: "8px 15px",
+              cursor: "pointer",
+            }}
+          >
+            Sign In
+          </button>
         </form>
         <p style={{ color: "#2a5ba6" }}>Forgot password?</p>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
           <hr style={{ width: "45%", margin: "0" }} />
           <span style={{ color: "#2a5ba6" }}>OR</span>
           <hr style={{ width: "45%", margin: "0" }} />
         </div>
         <p>Sign in with</p>
-        <div style={{ display: "flex", justifyContent: "center", marginBottom: "40px"  }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "40px",
+          }}
+        >
           <GoogleLogin
             onSuccess={responseGoogle}
             onFailure={() => {
@@ -112,4 +180,4 @@ const Login = () => {
       </div>
     </div>
   );
-	
+};
