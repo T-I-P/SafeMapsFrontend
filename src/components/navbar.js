5b4fba73d73b11522bc3ff702cfb79912a0c0f54
Deepanshu Sachdeva <56470354+deepanshusachdeva5@users.noreply.github.com>
2024-04-20 16:57:28 -0600

    Issue fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,10 +3,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./navbar.css";
+import React from "react";
 
 function NavigationBar() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Container className="navbar-container">
         <Navbar.Brand className="navlink-1" href="#home">
           Home
